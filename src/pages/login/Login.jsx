4461c0e0215d62d2c452cc20dd8fa4e1fe199a43
RallaPirate <tantegwen@gmail.com>
2025-04-26 18:36:05 +0200

    Login pagina afgemaakt, CSS opgeschoont, afgewerkt

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -26,57 +26,44 @@ function Login() {
             console.log("Login mislukt", error);
         }
     }
-// TODO: uitgecommente functie verwijderen als de JWT goed werkt!
-    // async function handleFormSubmit(data) {
-    //     const {email, password} = data;
-    //     console.log(data);
-    //
-    //     try {
-    //         const response = await axios.get('http://localhost:8080/posts/test', {
-    //             auth: {
-    //                 username: email,
-    //                 password: password,
-    //             }});
-    //         if (response.status === 200) {
-    //             console.log("Gelukt!!!");
-    //             navigate('/home');
-    //         }
-    //     }
-    //     catch (error) {
-    //         console.error("Login mislukt", error);
-    //         setLoginError("Inloggen mislukt.")
-    //     }
-    // }
 
     return (
         <>
             <HeaderLogin/>
             <div className="loginpage">
-            <p>Het SP Platform is de webapp waar je actief je stem kunt laten horen binnen de SP. <br/> Je kunt op deze website inloggen met je SP-account. Heb je daar hulp bij nodig? Stuur een mail naar webteam@sp.nl</p>
-            <form
-                className="loginform"
-                onSubmit={handleSubmit(handleFormSubmit)}>
-                <input
-                    type="email"
-                    placeholder="voer uw emailadres in"
-                    {...register("email", {
-                        required: true,
-                        message: "Emailadres is verplicht"
-                    } )} />
-                <input
-                    type="password"
-                    placeholder="voer uw wachtwoord in"
-                    {...register("password", {
-                        required: true,
-                        message: "Wachtwoord is verplicht"
-                    })}/>
-                <ButtonSubmit
-                text="inloggen"
-                size="large"
-                id="loginSubmit"
-                />
-                {loginError && <p>{loginError}</p>}
-            </form>
+                <form
+                    className="loginform"
+                    onSubmit={handleSubmit(handleFormSubmit)}>
+                    <div className="formIntro">
+                    <p>Het SP Platform is de webapp waar je actief je stem kunt laten horen binnen de SP. <br/> Je kunt
+                        op deze website inloggen met je SP-account. Heb je daar hulp bij nodig? Stuur een mail naar
+                        webteam@sp.nl</p>
+                    </div>
+                    <div className="formFields">
+                    <input
+                        type="email"
+                        placeholder="voer uw emailadres in"
+                        {...register("email", {
+                            required: true,
+                            message: "Emailadres is verplicht"
+                        })} />
+                    <input
+                        type="password"
+                        placeholder="voer uw wachtwoord in"
+                        {...register("password", {
+                            required: true,
+                            message: "Wachtwoord is verplicht"
+                        })}/>
+                    <ButtonSubmit
+                        text="inloggen"
+                        size="large"
+                        id="loginSubmit"
+                    />
+                    {loginError && <p>{loginError}</p>}
+                    </div>
+                    {/*Onderstaande div dients om flexbox zover te krijgen dat hij formInfo bovenaan en formFields in het midden weergeeft*/}
+                    <div></div>
+                </form>
             </div>
         </>
     )
