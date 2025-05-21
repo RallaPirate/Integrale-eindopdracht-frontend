b402e6472f05931121f10ac494cb0ea8f0307793
RallaPirate <tantegwen@gmail.com>
2025-05-21 20:13:04 +0200

    toggle werkt tussen posts en uploads op de profielpagina

--- src/components/uploadContent/UploadContent.jsx
+++ src/components/uploadContent/UploadContent.jsx
@@ -1,9 +1,17 @@
+import './UploadContent.css'
+import React from "react";
+
 function UploadContent({uploadcontent}) {
 console.log ("Ingeladen: ", uploadcontent);
     return (
-        <div>
-    <p>Filename: {uploadcontent.filename}</p>
+        <div className="uploadContent">
+            <img
+                src={`http://localhost:8080/api/profile/uploads/files/${uploadcontent.filename}`}
+            />
+            <div className="uploadContentText">
+            <p>Filename: {uploadcontent.filename}</p>
             <p>Id: {uploadcontent.profileUploadId}</p>
+            </div>
         </div>)
 }
 
